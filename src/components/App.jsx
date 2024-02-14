bd51e3823da6318904f472c2e93e8f7483002116
Nadine Estrella Vacas <nadinestrella@gmail.com>
2024-02-14 14:07:12 +0100

    initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,9 @@
+// Fichero src/components/App.jsxx
+import '../scss/App.scss';
+<styles />;
+
+function App() {
+  return <h1>My template</h1>;
+}
+
+export default App;
---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+// Fichero src/main.jsx
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './components/App.jsx';
+
+import { HashRouter } from 'react-router-dom';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  
+    <HashRouter>
+      <App />
+    </HashRouter>
+  
+);
