08ecbd6f0d5855c7a58b7b27f68b159a8ec9224d
Nadine Estrella Vacas <nadinestrella@gmail.com>
2024-07-24 11:47:42 +0200

    categories

--- src/app/components/Categories.tsx
+++ src/app/components/Categories.tsx
@@ -8,10 +8,6 @@ interface CategoriesProp {
   selectedCategories: string[];
 }
 
-// const getImageUrl = (path: string): string => {
-//   return `/images${path}`;
-// };
-
 export const Categories: React.FC<CategoriesProp> = ({
   uniqueCategories,
   onCateoriesChange,
@@ -19,22 +15,30 @@ export const Categories: React.FC<CategoriesProp> = ({
 }) => {
   const renderCategories = uniqueCategories.map((category) => {
     return (
-      <li key={category} className="containerCategories__li">
+      <li
+        key={category}
+        className="flex align-middle justify-center w-24 h-24 border-2 border-background3 rounded-full overflow-hidden   "
+      >
         <input
-          className="categories__input"
+          className="hidden"
           onChange={onCateoriesChange}
           type="checkbox"
           id={category}
           value={category}
           checked={selectedCategories.includes(category)}
         />
-        <label className="categories__checkbox" htmlFor={category}>
+        <label
+          className={`flex align-middle justify-center ${
+            selectedCategories.includes(category) ? 'grayscale-0' : 'grayscale'
+          }`}
+          htmlFor={category}
+        >
           <Image
-            className="categories__checkbox__image"
+            className=""
             src={`/categories/${category}.png`}
             alt="categories"
-            width={50}
-            height={50}
+            width={100}
+            height={100}
           />
         </label>
       </li>
@@ -44,7 +48,9 @@ export const Categories: React.FC<CategoriesProp> = ({
     <div className="flex flex-col justify-center items-center content-center gap-4">
       <h3>What do you like to play with? </h3>
       <p>Choose as many options as you like</p>
-      <ul className="grid grid-cols-2 gap-10">{renderCategories}</ul>
+      <ul className="grid grid-cols-2 md:grid-cols-3 gap-8">
+        {renderCategories}
+      </ul>
     </div>
   );
 };
