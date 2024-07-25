87453114ebc371a5e01547985584def02aab17df
Nadine Estrella Vacas <nadinestrella@gmail.com>
2024-07-25 15:44:08 +0200

    list toy

--- src/app/components/ListToy.tsx
+++ src/app/components/ListToy.tsx
@@ -19,33 +19,46 @@ export const ListToy: React.FC<ListToyProps> = ({
 }) => {
   const [toysList, setToysList] = useState<Toy[]>(toys);
 
+  const generateNewToysList = () => {
+    //filtra por cat y edad
+    const filteredToys = toys
+      .filter((toy) => filters.categories.includes(toy.category))
+      .filter((toy) => toy.age <= Number(filters.age));
+
+    //inicia una nueva lista de juguetes con los ya seleccionados
+    const newToysList = [...toysSelected];
+    // agrega juguetes adicionales aleatorios
+    const additionalToys = filteredToys
+      .filter(
+        (toy) => !toysSelected.some((selectedToy) => selectedToy.id === toy.id)
+      )
+      .sort(() => Math.random() - 0.5)
+      .slice(0, 6 - toysSelected.length);
+    //actualiza el estado con la nueva lista de juguetes
+    setToysList([...newToysList, ...additionalToys]);
+  };
+  //se llama automaticamente cuando cambian los filtros, los juguetes disponibles o los juguetes seleccionados
   useEffect(() => {
-    setToysList(
-      toys
-        .filter((toy) => filters.categories.includes(toy.category))
-        .filter((toy) => toy.age <= Number(filters.age))
-        .sort(() => Math.random() - 0.5)
-        .slice(0, 6)
-    );
-  }, [filters, toys]);
+    generateNewToysList();
+  }, [filters, toys, toysSelected]);
 
   return (
-    <div className="flex flex-col justify-center items-center content-center gap-4 pb-5">
+    <div className="flex flex-col justify-center items-center content-center gap-4 p-5">
       <div>
-        <h3>{kidName},</h3>
+        <p>{kidName},</p>
         <p>These are what we found for you!!</p>
         <p>Select the {toysList.length} toys you like the most. </p>
       </div>
       <div>
-        <ul className="p-1 grid grid-cols-2 gap-9 md:grid-cols-3">
+        <ul className="p-1 grid grid-cols-2 gap-9 md:grid-cols-3 md:gap-12">
           {toysList.map((toy, index) => {
             return (
               <li
                 key={index}
-                className="flex align-middle justify-center w-24 h-24"
+                className="flex flex-col align-middle justify-center"
               >
                 <label
-                  className={`w-28 h-auto md:h-32 border-2 border-background3 rounded-full overflow-hidden  hover:border-pink ${
+                  className={`w-28 h-28 md:w-32 md:h-32 border-2 border-background3 rounded-full overflow-hidden  hover:border-pink ${
                     toysSelected.map((toy) => toy.id).includes(toy.id)
                       ? 'border-pink border-8'
                       : 'border-background3'
@@ -62,26 +75,26 @@ export const ListToy: React.FC<ListToyProps> = ({
                     src={toy.image}
                     width={100}
                     height={100}
-                    alt="list toys"
+                    alt={`toy" ${toy.brand}`}
+                    className="w-full h-full object-cover rounded-full"
                   />
-
-                  <p className="text-sm font-bold hidden md:block">
-                    {toy.model.length > 15
-                      ? toy.model.slice(0, 15) + '...'
-                      : toy.model}
-                  </p>
                 </label>
+                <p className="text-sm font-bold hidden md:block">
+                  {toy.model.length > 15
+                    ? toy.model.slice(0, 15) + '...'
+                    : toy.model}
+                </p>
               </li>
             );
           })}
         </ul>
       </div>
       <div>
-        <p className="listToyAgain">
+        <p className="flex flex-row gap-4">
           Any match?{' '}
           <button
-            onClick={() => setToysList(getToysFilteredByCategAndAge())}
-            className="tryAgainBtn"
+            onClick={generateNewToysList}
+            className="w-28 h-6 bg-background3 text-white border-0 rounded-full hover:bg-nextBtn"
           >
             Try again
           </button>
