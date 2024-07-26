a3ff9bfe1d33b5107b8a646e8d14e8f32e0ea30c
Nadine Estrella Vacas <nadinestrella@gmail.com>
2024-07-26 17:12:03 +0200

    footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,9 @@ import { Inter } from 'next/font/google';
 import './globals.css';
 import { Header } from './components/Header';
 import { Footer } from './components/Footer';
+import '@fortawesome/fontawesome-svg-core/styles.css';
+import { config } from '@fortawesome/fontawesome-svg-core';
+config.autoAddCss = false;
 
 const inter = Inter({ subsets: ['latin'] });
 
