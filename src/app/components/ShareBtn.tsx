3dc2e6a79cb8ddb799f6d871cd00825b3f83b6b8
Nadine Estrella Vacas <nadinestrella@gmail.com>
2024-09-12 13:30:53 +0200

    share

---
+++ src/app/components/ShareBtn.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+// const shareData = {
+//   title: 'Share',
+//   text: 'Look what I have found!',
+//   url: 'https://developer.mozilla.org',
+// };
+
+// export const ShareBtn = () => {
+//   const handleShare = async () => {
+//     if (navigator.share) {
+//       try {
+//         await navigator.share(shareData);
+//       } catch (err) {}
+//     } else {
+//       alert('Web Share API is not supported in this browser.');
+//     }
+//   };
+
+//   return (
+//     <div>
+//       <button onClick={handleShare}>Share</button>
+//       <p>datos</p>
+//     </div>
+//   );
+// };
