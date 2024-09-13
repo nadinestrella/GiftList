2c74cf8400d8703c996ecf8703d66af093b6ab1f
Nadine Estrella Vacas <nadinestrella@gmail.com>
2024-09-13 20:16:34 +0200

    localStorage

---
+++ src/app/services/localStorage.tsx
@@ -0,0 +1,31 @@
+const setInLocalStorage = (key: string, value: any) => {
+  const stringifyData = JSON.stringify(value);
+  localStorage.setItem('data', stringifyData);
+};
+
+const getFromLocalStorage = (key: string, defaultValue: any) => {
+  const localStorageData = localStorage.getItem('data');
+  if (localStorageData === null) {
+    return defaultValue;
+  } else {
+    return JSON.parse(localStorageData);
+  }
+};
+
+const removeFromLocalStorage = (key: string) => {
+  localStorage.removeItem(key);
+};
+
+const clearFromLocalStorage = () => {
+  localStorage.clear();
+};
+
+const objectToExport = {
+  setInLocalStorage: setInLocalStorage,
+  getFromLocalStorage: getFromLocalStorage,
+  removeFromLocalStorage: removeFromLocalStorage,
+  clearFromLocalStorage: clearFromLocalStorage,
+};
+
+// Exportamos el objeto para que pueda ser usado desde App
+export default objectToExport;
